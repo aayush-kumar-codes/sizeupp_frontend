4bf7020c5bba54ec3307ab813ba13f5fbb1f2205
stalwartszen <vu1f1920018@pvppcoe.ac.in>
2023-12-24 13:59:08 +0530

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -277,7 +277,7 @@ export function Login() {
                                 </div>
                             </div>
                         </form>
-                        <div className="mt-3 space-y-3">
+                        {/* <div className="mt-3 space-y-3">
                             <button
                                 type="button"
                                 className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
@@ -295,7 +295,7 @@ export function Login() {
                                 Sign in with Google
                             </button>
 
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
