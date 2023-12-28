749436c461417eea7cde77b76aebf7ebcef1d19b
stalwartszen <vu1f1920018@pvppcoe.ac.in>
2023-12-28 16:08:11 +0530

    -- nan

--- src/pages/ProductOverview.jsx
+++ src/pages/ProductOverview.jsx
@@ -1242,45 +1242,45 @@ const ProductOverview = () => {
                   <div className=" col-span-1 grid grid-rows-3 gap-y-6 border-r border-black/60">
                     <div className="row-span-1 px-2">
                       <h3 className="text-base text-gray-800/80 font-semibold">Occasion</h3>
-                      <p className="text-lg">{demo.product?.occasion || "Occasion"}</p>
+                      <p className="text-lg">{demo.product?.occasion != "nan" ? demo.product?.occasion : '--' || "Occasion"}</p>
                     </div>
                     <div className="row-span-1 px-2">
                       <h3 className="text-base text-gray-800/80 font-semibold">Sleeve Type</h3>
-                      <p className="text-lg">{demo.product?.sleeve || "Sleeve"}</p>
+                      <p className="text-lg">{demo.product?.sleeve != "nan" ? demo.product.sleeve : "--" || "Sleeve"}</p>
                     </div>
                     <div className="row-span-1 px-2">
                       <h3 className="text-base text-gray-800/80 font-semibold">Fit</h3>
-                      <p className="text-lg">{demo.product?.fit || "Fit"}</p>
+                      <p className="text-lg">{demo.product?.fit != "nan" ? demo.product.fit : "--" || "Fit"}</p>
                     </div>
                     <div className="row-span-1 px-2">
                       <h3 className="text-base text-gray-800/80 font-semibold">Origin</h3>
                       <p className="text-lg">MADE IN INDIA</p>
                     </div>
                     <div className="row-span-1 px-2">
-                      <h3 className="text-base text-gray-800/80 font-semibold"></h3>
-                      <p className="text-lg">{""}</p>
+                      <h3 className="text-base text-gray-800/80 font-semibold"> </h3>
+                      <p className="text-lg">{" "}</p>
                     </div>
                   </div>
                   <div className=" col-span-1 grid grid-rows-3 gap-y-6">
                     <div className="row-span-1 px-2">
                       <h3 className="text-base text-gray-800/80 font-semibold">Material</h3>
-                      <p className="text-lg">{demo.product?.fabric_detail}</p>
+                      <p className="text-lg">{demo.product?.fabric_detail != "nan" ? demo.product?.fabric_detail : "--"}</p>
                     </div>
                     <div className="row-span-1 px-2">
                       <h3 className="text-base text-gray-800/80 font-semibold">Design/Surface</h3>
-                      <p className="text-lg">{demo.product?.design_surface}</p>
+                      <p className="text-lg">{demo.product?.design_surface != "nan" ? demo.product.design_surface : "--"}</p>
                     </div>
                     <div className="row-span-1 px-2">
                       <h3 className="text-base text-gray-800/80 font-semibold">Neckline Type</h3>
-                      <p className="text-lg">{demo.product?.neck_type}</p>
+                      <p className="text-lg">{demo.product?.neck_type != "nan" ? demo.product.neck_type : "--"}</p>
                     </div>
                     <div className="row-span-1 px-2">
                       <h3 className="text-base text-gray-800/80 font-semibold">Color Family</h3>
-                      <p className="text-lg">{demo.product?.cf}</p>
+                      <p className="text-lg">{demo.product?.cf != "nan" ? demo.product.cf : "--"}</p>
                     </div>
                     <div className="row-span-1 px-2">
                       <h3 className="text-base text-gray-800/80 font-semibold">Style</h3>
-                      <p className="text-lg">{demo.product?.style}</p>
+                      <p className="text-lg">{demo.product?.style != "nan" ? demo.product.style : "--"}</p>
                     </div>
                   </div>
                 </div>
