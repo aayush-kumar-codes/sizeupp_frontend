283949b77caf2399e4496b00a3e2be73309a552a
pratikd2124 <pratikd2124@gmail.com>
2023-12-30 15:43:33 +0530

    minor

--- src/pages/ProductOverview.jsx
+++ src/pages/ProductOverview.jsx
@@ -963,8 +963,30 @@ const ProductOverview = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
               </svg>
 
-              <a href="#" className="ml-1 text-base text-c-gray-800 hover:underline md:ml-2">
+              <a href={`/products?`} className="ml-1 text-base text-c-gray-800 hover:underline md:ml-2">
                 {demo.product?.category.name}
+              </a>
+            </div>
+          </li>
+          <li>
+            <div className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+              </svg>
+
+              <a href={`/products?`} className="ml-1 text-base text-c-gray-800 hover:underline md:ml-2">
+                {demo.product?.subcategory.name}
+              </a>
+            </div>
+          </li>
+          <li>
+            <div className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+              </svg>
+
+              <a href={`/products?`} className="ml-1 text-base text-c-gray-800 hover:underline md:ml-2">
+                {demo.product?.subsubcategory.name}
               </a>
             </div>
           </li>
