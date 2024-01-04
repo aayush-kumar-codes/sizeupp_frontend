063f3c69395ac3c8edc0c02e6c70ad5373ac93b4
stalwartszen <vu1f1920018@pvppcoe.ac.in>
2024-01-04 09:18:04 +0530

    Add react-router-dom to MobileNav component

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 import { HomeIcon,ShoppingCartIcon,HeartIcon,UserIcon   } from "@heroicons/react/24/outline";
 import {ArrowUpIcon } from "@heroicons/react/24/outline";
+import { Link } from 'react-router-dom';
 
 const ScrollButton = () =>{ 
   
@@ -45,25 +46,25 @@ const MobileNav = () => {
 {/* <!-- fixed nav --> */}
     <div className="md:hidden z-50 fixed bottom-0 inset-x-0 bg-[#FFAE00] flex justify-between text-xs  uppercase font-mono">
 
-    <a href="/" className="w-full flex flex-col  py-2 px-3 text-center hover:bg-yellow-200 hover:text-yellow-800 transition duration-300">
+    <Link to="/" className="w-full flex flex-col  py-2 px-3 text-center hover:bg-yellow-200 hover:text-yellow-800 transition duration-300">
             <HomeIcon className="h-5 w-5 mx-auto text-yellow-800" />
         Home
-    </a>
+    </Link>
 
-    <a href="/products/cart" className="w-full flex flex-col  py-2 px-3 text-center hover:bg-yellow-200 hover:text-yellow-800">
+    <Link to="/products/cart" className="w-full flex flex-col  py-2 px-3 text-center hover:bg-yellow-200 hover:text-yellow-800">
         <ShoppingCartIcon className="h-5 w-5 mx-auto text-yellow-800" />
         Cart
-    </a>
+    </Link>
 
-    <a href="/products/favourite" className="w-full flex flex-col  py-2 px-3 text-center hover:bg-yellow-200 hover:text-yellow-800">
+    <Link to="/products/favourite" className="w-full flex flex-col  py-2 px-3 text-center hover:bg-yellow-200 hover:text-yellow-800">
         <HeartIcon className="h-5 w-5 mx-auto text-yellow-800" />
         Wishlist
-    </a>
+    </Link>
 
-    <a href="/profile" className="w-full flex flex-col  py-2 px-3 text-center hover:bg-yellow-200 hover:text-yellow-800">
+    <Link to="/profile" className="w-full flex flex-col  py-2 px-3 text-center hover:bg-yellow-200 hover:text-yellow-800">
         <UserIcon className="h-5 w-5 mx-auto text-yellow-800" />
         Profile
-    </a>
+    </Link>
 
     </div>
       </>
