1730243812b0c2202893165988c78674ade9b07d
stalwartszen <vu1f1920018@pvppcoe.ac.in>
2024-01-04 16:13:56 +0530

    Add meta description for Sizeupp website and reset form data on close button click in ManageAddress component. Update navigation logic in ProductCart component.

--- src/pages/Account/ManageAddress.jsx
+++ src/pages/Account/ManageAddress.jsx
@@ -427,7 +427,14 @@ const NewAddress = () => {
               <button type="button" onClick={(e) => { handleSubmit(e); setIsEdit(false); }} className="rounded-lg bg-blue-500 text-white px-4 py-2">
                 Save
               </button>
-              <button type="button" onClick={() => { setIsOpen(false) }} className="rounded-lg bg-red-500 text-white px-4 py-2">
+              <button type="button" onClick={() => { setIsOpen(false); setFormData({
+                addressid: "",
+                addressLine1: '',
+                addressLine2: '',
+                city: '',
+                state: "",
+                pinCode : ""
+              }) }} className="rounded-lg bg-red-500 text-white px-4 py-2">
                 Close
               </button>
             </div>
--- src/pages/ProductCart.jsx
+++ src/pages/ProductCart.jsx
@@ -854,13 +854,9 @@ export function ProductCart() {
                                 <button
                                     type="button"
                                     onClick={() => {
-                                        if (cart) {
-                                            window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-                                            return navigate('/products')
-                                        }
-                                        else {
+                                        
                                             navigate('/products/billing')
-                                        }
+                                        
                                     }}
                                     className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                                 >
