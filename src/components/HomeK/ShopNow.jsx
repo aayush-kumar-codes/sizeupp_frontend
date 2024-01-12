ca3aff2305580fa0211b607b29dfdb87da3b6ecf
stalwartszen <vu1f1920018@pvppcoe.ac.in>
2024-01-12 16:51:02 +0530

    Fix typos in ShopNow component

--- src/components/HomeK/ShopNow.jsx
+++ src/components/HomeK/ShopNow.jsx
@@ -7,14 +7,14 @@ export default function ShopNow() {
       <>
         
         <Layout 
-          Text="Men`s Ethnic Wears"
+          Text="Men's Ethnic Wears"
                 Image={banner6}
                 smImg={mobile4}
                 Orientation={false}
                 href={`/products?gender=56331b14-95bb-4d6f-8fbd-eaf228d25a27&category=db93ce20-7baf-4ebd-be17-89d1215a04c9&subcategory=7f342ad1-b316-40f0-841f-01ec0b31ffbb`}
         />
         <Layout 
-          Text="Women`s Formal Wear"
+          Text="Women's Formal Wear"
                 Image={banner8}
                 smImg={mobile6}
                 Orientation={false}
@@ -55,7 +55,7 @@ const Layout = ({ Text, Image,smImg, Orientation,href }) => {
 
             </div>
             <div className='w-2/3 h-full flex flex-col md:justify-center justify-between items-center text-center z-10 gap-10 lg:p-0 py-16'>
-                <h1 className="font-normal lg:text-[3.8rem] capitalize font-serif text-white text-4xl">{Text}</h1>
+                <h1 className="font-normal lg:text-[3.8rem] font-serif text-white text-4xl">{Text}</h1>
                 <Link to={href}>
                 <button className="border-2 border-white text-white w-48 h-16 text-2xl md:w-36 md:h-12 md:text-lg sm:w-28 sm:h-8 sm:text-base">Shop Now</button>
 
